da7a883b23a11dc7ffe37eb968e6a52f1f8e9a9a
HaiyangDING <dinghaiyang@huawei.com>
2016-04-17 08:37:28 +0800

    Lab1 Part3: test not passed

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -2,16 +2,12 @@ package mapreduce
 
 import (
 	"container/list"
-	"sync"
 )
 import "fmt"
 
 type WorkerInfo struct {
 	address string
 	// You can add definitions here.
-
-	// add any additional item here
-	state int // 0 means ready
 }
 
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
@@ -35,21 +31,78 @@ func (mr *MapReduce) KillWorkers() *list.List {
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
 	fmt.Print("Start RunMaster() here...")
-	
-	// register workers from chanel
-	go func() {
-		for {
-			workerAdd := <- mr.registerChannel
-			mr.mu.Lock()
-			mr.Workers[workerAdd] = &WorkerInfo{workerAdd, 0}
-			mr.mu.Unlock()
-			fmt.Print("Worker %v registered.\n", workerAdd)
+
+	// Defining the channel
+	idleWorkerChan := make(chan string)
+	jobChan := make(chan *DoJobArgs)
+	jobDoneChan := make(chan int)
+
+	//find an idle worker: by registration or when job finishes
+	getNextWorker := func() string {
+		var address string
+
+		select {
+		case address = <-mr.registerChannel:
+			mr.Workers[address] = &WorkerInfo{address}
+		case address = <-idleWorkerChan:
+		}
+
+		return address
+	}
+
+	doJob := func(worker string, job *DoJobArgs) {
+		var reply DoJobReply
+
+		jobDone := call(worker, "Worker.DoJob", job, &reply)
+
+		if jobDone {
+			jobDoneChan <- 1
+			idleWorkerChan <- worker
+		} else {
+			fmt.Printf("RunMaster RPC call error with worker: %s ... Redistributing the job", worker)
+			jobChan <- job
 		}
 	}
-	
-	// handle job to each worker and wait until complete
-	var wg sync.WaitGroup
-	
+
+	// start job distributing, for either Map or Reduce
+	// remember to close the jobChan when Reduce's are all done
+	go func() {
+		for job := range jobChan {
+			worker := getNextWorker()
+			go func(job *DoJobArgs) {
+				doJob(worker, job)
+			}(job)
+		}
+	}()
+
+	// start adding Map job to jobChan
+	go func() {
+		for i := 0; i < mr.nMap; i++ {
+			job := &DoJobArgs{mr.file, Map, i, mr.nMap}
+			jobChan <- job
+		}
+	}()
+
+	// wait for the number of Map's are done
+	for i := 0; i < mr.nMap; i++ {
+		<-jobDoneChan
+	}
+
+	// start adding Reduce job to jobChan
+	go func() {
+		for i := 0; i < mr.nReduce; i++ {
+			job := &DoJobArgs{mr.file, Reduce, i, mr.nReduce}
+			jobChan <- job
+		}
+	}()
+
+	// wait for the number of Reduce's are done
+	for i := 0; i < mr.nReduce; i++ {
+		<-jobDoneChan
+	}
+
+	// close jobChan so that the jobChan loop could finish
+	close(jobChan)
 
 	return mr.KillWorkers()
 }
